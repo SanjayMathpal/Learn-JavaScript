ad203bac92bf8c6bee15471286174e8ea672944e
Sanjay Mathpal <96293695+SanjayMathpal@users.noreply.github.com>
2024-02-27 17:01:18 +0000

    var let const

---
+++ learnJS/first.js
@@ -0,0 +1,14 @@
+var name = "Sanjay"
+let age = 21
+const id = 2061921
+
+name = "Name Changed" // name changed because var means global scope
+console.log(name);
+
+// age = 20 cannot change because of the same scope
+
+console.log(age)
+
+// id = 20011360 error due to contant keyword
+
+console.log(id);
